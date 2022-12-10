2a947cba1eaac749a9961d934dbbb5b931872371
Leonard Yu <hyu@xenanetworks.com>
2022-12-10 22:06:07 +0100

    build: Add build files

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+def main():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    setuptools.setup(
+        name="xoa-driver",
+        description=(
+            "Xena Utility is a driver providing user-friendly communication"
+            " interfaces to Xena's physical and virtual Traffic Generation and Analysis (TGA) testers."
+            " It provides a rich collection of programming interfaces that can be used to either write"
+            " test scripts or develop applications."
+        ),
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Ron Ding, Leonard Yu",
+        author_email="rdi@xenanetworks.com, hyu@xenanetworks.com",
+        maintainer="Xena Networks",
+        maintainer_email="support@xenanetworks.com",
+        url="https://github.com/xenanetworks/open-automation-utility",
+        packages=setuptools.find_packages(),
+        license='Apache 2.0',
+        install_requires=["typing_extensions", "loguru"],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+        ],
+        python_requires=">=3.8",
+    )
+
+
+if __name__ == '__main__':
+    main()
